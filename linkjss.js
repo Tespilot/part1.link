2d4b2c3e3f09e86c1ec1025d6dd428dc02466a20
Tespilot <90264541+Tespilot@users.noreply.github.com>
2021-10-29 12:34:19 +0530

    Add files via upload

---
+++ linkjss.js
@@ -0,0 +1,7 @@
+function message(a) {
+    console.log("Hello" + a);
+    window.alert("Welcome");
+    document.getElementById("mess").innerHTML= " Linking js to html file successful";
+    
+}
+message("Tejas");
